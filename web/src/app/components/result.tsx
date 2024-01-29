65a76aad5cad2b7810bd14f71d9b6920c987268a
Alex Shan <shan-mx@hotmail.com>
2024-01-29 17:16:45 +0800

    fix: react useeffect exxcutes twice

--- web/src/app/components/result.tsx
+++ web/src/app/components/result.tsx
@@ -1,19 +1,22 @@
 "use client";
 import { Answer } from "@/app/components/answer";
-import { Relates } from "@/app/components/relates";
 import { Sources } from "@/app/components/sources";
-import { Relate } from "@/app/interfaces/relate";
 import { Source } from "@/app/interfaces/source";
 import { parseStreaming } from "@/app/utils/parse-streaming";
 import { Annoyed } from "lucide-react";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState, useRef } from "react";
 
 export const Result: FC<{ query: string; rid: string }> = ({ query, rid }) => {
   const [sources, setSources] = useState<Source[]>([]);
   const [markdown, setMarkdown] = useState<string>("");
-  const [relates, setRelates] = useState<Relate[] | null>(null);
   const [error, setError] = useState<number | null>(null);
+  const queryRef = useRef(true);
+
   useEffect(() => {
+    if (queryRef.current) {
+      queryRef.current = false;
+      return;
+    }
     const controller = new AbortController();
     void parseStreaming(
       controller,
@@ -21,7 +24,6 @@ export const Result: FC<{ query: string; rid: string }> = ({ query, rid }) => {
       rid,
       setSources,
       setMarkdown,
-      setRelates,
       setError,
     );
     return () => {
@@ -32,7 +34,6 @@ export const Result: FC<{ query: string; rid: string }> = ({ query, rid }) => {
     <div className="flex flex-col gap-8">
       <Answer markdown={markdown} sources={sources}></Answer>
       <Sources sources={sources}></Sources>
-      <Relates relates={relates}></Relates>
       {error && (
         <div className="absolute inset-4 flex items-center justify-center bg-white/40 backdrop-blur-sm">
           <div className="p-4 bg-white shadow-2xl rounded text-blue-500 font-medium flex gap-4">
